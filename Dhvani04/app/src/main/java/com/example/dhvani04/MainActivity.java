099e952945dc9040da63d50b6e6e4e2eefa6a88a
DhvaniThummar <87126536+DhvaniThummar@users.noreply.github.com>
2023-03-26 17:24:49 +0530

    Add files via upload

---
+++ Dhvani04/app/src/main/java/com/example/dhvani04/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dhvani04;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
